3903fe8a54de0588965c27b20ab009b024565873
Junichi Hashimoto <junichi_hashimoto@hotmail.co.jp>
2020-08-29 16:49:58 +0900

    created project

---
+++ JHashimoto.ClipboardTinyTools/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace JHashimoto.ClipboardTinyTools {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
